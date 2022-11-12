ffbc9bdedf87e6e2fed602d4b46c342c037dbc12
Dakioss <f.gonzlezfelix@outlook.com>
2022-11-12 08:34:23 -0300

    changes 6

--- src/features/restaurants/screens/restaurant-detail.screen.js
+++ src/features/restaurants/screens/restaurant-detail.screen.js
@@ -7,7 +7,17 @@ import { RestaurantInfoCard } from "../components/restaurant-info-card.component
 import { SafeArea } from "../../../components/utility/safe-area.component";
 
 export const RestaurantDetailScreen = ({ route }) => {
-  const [breakfastExpanded, setBreakfastExpanded] = useState(false);
+  const [semana1Expanded, setSemana1Expanded] = useState(false);
+  const [semana2Expanded, setSemana2Expanded] = useState(false);
+  const [semana3Expanded, setSemana3Expanded] = useState(false);
+  const [semana4Expanded, setSemana4Expanded] = useState(false);
+  const [semana5Expanded, setSemana5Expanded] = useState(false);
+  const [semana6Expanded, setSemana6Expanded] = useState(false);
+  const [semana7Expanded, setSemana7Expanded] = useState(false);
+  const [semana8Expanded, setSemana8Expanded] = useState(false);
+  const [semana9Expanded, setSemana9Expanded] = useState(false);
+  const [semana10Expanded, setSemana10Expanded] = useState(false);
+  const [semana11Expanded, setSemana11Expanded] = useState(false);
   const [lunchExpanded, setLunchExpanded] = useState(false);
   const [dinnerExpanded, setDinnerExpanded] = useState(false);
   const [drinksExpanded, setDrinksExpanded] = useState(false);
@@ -20,8 +30,8 @@ export const RestaurantDetailScreen = ({ route }) => {
         <List.Accordion
           title="Semana 1"
           left={(props) => <List.Icon {...props} icon="book" />}
-          expanded={breakfastExpanded}
-          onPress={() => setBreakfastExpanded(!breakfastExpanded)}
+          expanded={semana1Expanded}
+          onPress={() => setSemana1Expanded(!semana1Expanded)}
         >
           <List.Item title="Contenido semana 1" />
         </List.Accordion>
@@ -29,8 +39,8 @@ export const RestaurantDetailScreen = ({ route }) => {
         <List.Accordion
           title="Semana 2"
           left={(props) => <List.Icon {...props} icon="book" />}
-          expanded={lunchExpanded}
-          onPress={() => setLunchExpanded(!lunchExpanded)}
+          expanded={semana2Expanded}
+          onPress={() => setSemana2Expanded(!semana2Expanded)}
         >
           <List.Item title="Contenido semana 2" />
         </List.Accordion>
@@ -38,8 +48,8 @@ export const RestaurantDetailScreen = ({ route }) => {
         <List.Accordion
           title="Semana 3"
           left={(props) => <List.Icon {...props} icon="book" />}
-          expanded={lunchExpanded}
-          onPress={() => setLunchExpanded(!lunchExpanded)}
+          expanded={semana3Expanded}
+          onPress={() => setSemana3Expanded(!semana3Expanded)}
         >
           <List.Item title="Contenido semana 3" />
         </List.Accordion>
@@ -47,8 +57,8 @@ export const RestaurantDetailScreen = ({ route }) => {
         <List.Accordion
           title="Semana 4"
           left={(props) => <List.Icon {...props} icon="book" />}
-          expanded={lunchExpanded}
-          onPress={() => setLunchExpanded(!lunchExpanded)}
+          expanded={semana4Expanded}
+          onPress={() => setSemana4Expanded(!semana4Expanded)}
         >
           <List.Item title="Contenido semana 4" />
         </List.Accordion>
@@ -56,8 +66,8 @@ export const RestaurantDetailScreen = ({ route }) => {
         <List.Accordion
           title="Semana 5"
           left={(props) => <List.Icon {...props} icon="book" />}
-          expanded={lunchExpanded}
-          onPress={() => setLunchExpanded(!lunchExpanded)}
+          expanded={semana5Expanded}
+          onPress={() => setSemana5Expanded(!semana5Expanded)}
         >
           <List.Item title="Contenido semana 5" />
         </List.Accordion>
@@ -65,8 +75,8 @@ export const RestaurantDetailScreen = ({ route }) => {
         <List.Accordion
           title="Semana 6"
           left={(props) => <List.Icon {...props} icon="book" />}
-          expanded={lunchExpanded}
-          onPress={() => setLunchExpanded(!lunchExpanded)}
+          expanded={semana6Expanded}
+          onPress={() => setSemana6Expanded(!semana6Expanded)}
         >
           <List.Item title="Contenido semana 6" />
         </List.Accordion>
@@ -74,8 +84,8 @@ export const RestaurantDetailScreen = ({ route }) => {
         <List.Accordion
           title="Semana 7"
           left={(props) => <List.Icon {...props} icon="book" />}
-          expanded={lunchExpanded}
-          onPress={() => setLunchExpanded(!lunchExpanded)}
+          expanded={semana7Expanded}
+          onPress={() => setSemana7Expanded(!semana7Expanded)}
         >
           <List.Item title="Contenido semana 7" />
         </List.Accordion>
@@ -83,8 +93,8 @@ export const RestaurantDetailScreen = ({ route }) => {
         <List.Accordion
           title="Semana 8"
           left={(props) => <List.Icon {...props} icon="book" />}
-          expanded={lunchExpanded}
-          onPress={() => setLunchExpanded(!lunchExpanded)}
+          expanded={semana8Expanded}
+          onPress={() => setSemana8Expanded(!semana8Expanded)}
         >
           <List.Item title="Contenido semana 8" />
         </List.Accordion>
@@ -92,8 +102,8 @@ export const RestaurantDetailScreen = ({ route }) => {
         <List.Accordion
           title="Semana 9"
           left={(props) => <List.Icon {...props} icon="book" />}
-          expanded={lunchExpanded}
-          onPress={() => setLunchExpanded(!lunchExpanded)}
+          expanded={semana9Expanded}
+          onPress={() => setSemana9Expanded(!semana9Expanded)}
         >
           <List.Item title="Contenido semana 9" />
         </List.Accordion>
@@ -101,8 +111,8 @@ export const RestaurantDetailScreen = ({ route }) => {
         <List.Accordion
           title="Semana 10"
           left={(props) => <List.Icon {...props} icon="book" />}
-          expanded={lunchExpanded}
-          onPress={() => setLunchExpanded(!lunchExpanded)}
+          expanded={semana10Expanded}
+          onPress={() => setSemana10Expanded(!semana10Expanded)}
         >
           <List.Item title="Contenido semana 10" />
         </List.Accordion>
@@ -110,8 +120,8 @@ export const RestaurantDetailScreen = ({ route }) => {
         <List.Accordion
           title="Semana 11"
           left={(props) => <List.Icon {...props} icon="book" />}
-          expanded={lunchExpanded}
-          onPress={() => setLunchExpanded(!lunchExpanded)}
+          expanded={semana11Expanded}
+          onPress={() => setSemana11Expanded(!semana11Expanded)}
         >
           <List.Item title="Contenido semana 11" />
         </List.Accordion>
